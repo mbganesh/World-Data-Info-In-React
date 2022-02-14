eeaaa2351b4d7b87e14e29d644d0b4a6c245f0b7
SharedPreferences <boothaganesh.jdev@gmail.com>
2022-02-14 11:03:22 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import axios from "axios";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [myJSONList, setMyJSONList] = useState([]);
+
+  const getJSON = () => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      let result = res.data;
+      setMyJSONList(result);
+      console.log(result[0].maps.googleMaps);
+    });
+  };
+
+  useEffect(() => {
+    getJSON();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{justifyContent:'center' , alignItems:'center' , display:'flex' , width:'99vw' , flexDirection:'column'}}>
+      {myJSONList.map((data, index) => (
+        <div
+          style={{
+            border: "2px solid #2C3333",
+            borderRadius: "10px",
+            backgroundColor: "#00B4D8",
+            margin: "5px",
+            width:'48vw',
+            color: "white"
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{ backgroundColor: "#03045E" }}>
+            <h1 style={{ textAlign: "center" }}> {data.name.common} </h1>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginLeft: "15px",
+                marginRight: "15px"
+              }}
+            >
+              <h2> {data.name.official} </h2>
+              <span style={{ display: "flex" , justifyContent:'center' , alignItems:'center' }}>
+                <h3 > Population : </h3> <h2> {data.population} </h2>{" "}
+              </span>
+            </div>
+          </div>
+
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+              <h4 style={{color:'#010101'}}>Flag</h4>
+              <img
+                src={data.flags.png}
+                style={{ width: "150px", height: "150px" }}
+              />
+            </div>
+
+            <div>
+              <h4 style={{color:'#010101'}} > Coat Of Arms</h4>
+              <img
+                src={data.coatOfArms.png}
+                style={{ width: "150px", height: "150px" }}
+              />
+            </div>
+          </div>
+
+          <div>
+              <h3 style={{color:'#010101'}}> Continents </h3>
+              <ul>
+                <li> {data.continents} </li>
+                </ul>
+            </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              margin: "15px"
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                window.open(data.maps.googleMaps);
+              }}
+            >
+              {" "}
+              Google Map{" "}
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => {
+                window.open(data.maps.openStreetMaps);
+              }}
+            >
+              Open Street Map{" "}
+            </Button>
+          </div>
+
+          {/* <div>
+            <iframe
+              src={data.maps.googleMaps}
+              height={500}
+              width={500}
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+            ></iframe>
+          </div> */}
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
